cc6c5996543bdb8f40f3c0966cc09a9aa6fb8de1
Calvin Belden <cbelden@nd.edu>
2014-06-27 09:11:06 -0500

    Updated router so the 'track' parameter is specified in the URL. A new producer is created for each URL (meant for a single user).

--- twitter_dash.py
+++ twitter_dash.py
@@ -1,17 +1,34 @@
 from flask import Flask, render_template
 from flask.ext.socketio import SocketIO, request
 from twitter_dash import Consumer, TwitterFeedProducer
+from collections import namedtuple
 
 
 app = Flask(__name__)
+app.config['DEBUG'] = True
+twitter_stream = {'track': None, 'producer': None}
 app.config['SECRET_KEY'] = 'secret' # TODO: find out what this is used for
 socketio =  SocketIO(app)
 
 
-@app.route('/')
-def index():
+@app.route('/<tracking>')
+def index(tracking):
 	"""Renders index.html."""
-	return render_template('index.html', filtering='usa')
+
+	# Kill current twitter stream
+	if twitter_stream['producer']:
+		twitter_stream['producer'].kill()
+
+	# Assign new twitter stream
+	twitter_stream['track'] = tracking
+	twitter_stream['producer'] = TwitterFeedProducer([tracking])
+
+	# Start the twitter producer
+	twitter_stream['producer'].start()
+
+	print twitter_stream
+
+	return render_template('index.html', track=tracking)
 
 @socketio.on('connect', namespace='/twitter')
 def on_connect():
@@ -33,9 +50,6 @@ def on_filter_request(msg):
 
 if __name__ == '__main__':
 
-	# Start the twitter producer
-	producer = TwitterFeedProducer(['usa'])
-	producer.start()
 
 	# Run the web service
 	socketio.run(app)
