962d2ec162a0943b1f6e0846957a974402dfe999
Calvin Belden <cbelden@nd.edu>
2014-07-10 16:10:35 -0500

    Made _tracking dict to store each user's tracking terms (because socketio handlers cannot modify the session variable. Updated _kill_stream() to check greenlet's .successful() value.

--- twitter_dash.py
+++ twitter_dash.py
@@ -17,24 +17,25 @@ MAX_INSTANCES_PER_USER = 3
 _producers = {}        	# Stores each Producer service
 _consumers = {}        	# Stores each user's Consumer service
 _namespaces = {}		# Stores a list of each user's namespace (used for the socket location)
+_tracking = {}          # Stores the terms each user is tracking
 
 
 def _create_producer():
     """Creates a new Producer stream for the terms specified in tracking."""
 
     # Get user's tracking terms and username
-    tracking = session['tracking']
-    terms = frozenset(tracking)
     user = session['username']
+    tracking = _tracking[user]
+    terms = frozenset(tracking)
 
     # Remove this client from other streams
-    for p_terms, producer in _producers.iteritems():
+    for _terms, producer in _producers.iteritems():
         if user in producer.clients:
             if producer.tracking is not tracking:
                 producer.remove_client(user)
 
     # Create a new producer if it doesn't exist or it has been killed.
-    if terms not in _producers or _producers[terms].value:
+    if terms not in _producers or _producers[terms].successful():
         # Create a Twitter stream Producer (that filters on the term 'tracking')
         feed_producer = StreamProducer(tracking)
         feed_producer.start()
@@ -48,8 +49,8 @@ def _create_consumer():
     """Creates a new redis pub/sub consumer that subscribes to correct channel."""
 
     # Get user information
-    tracking = session['tracking']
     username = session['username']
+    tracking = _tracking[username]
     emit = _namespaces[username].emit
 
     # Kill previous consumer (each user only gets one instance)
@@ -57,25 +58,46 @@ def _create_consumer():
     	_consumers[username].kill()
 
 	# Create new Consumer
-    channel = json.dumps(sorted(tracking))
+    channel = json.dumps(sorted(tracking))      # Redis channel name the consumer subscribes to
     feed_consumer = StreamConsumer(channel, emit)
     feed_consumer.start()
     _consumers[username] = feed_consumer
 
 def _kill_stream():
-    """Kills the current user's Twitter consumer."""
+    """Kills the current user's Twitter consumer and notifies producer."""
 
     user = session['username']
     consumer = _consumers.get(user, None)
-    terms = frozenset(session['tracking'])
-    producer = _producers.get(terms, None)
+    tracking = _tracking.get(user, None)
+    print "In kill. tracking: ", tracking
 
+    # If the user has tracking terms AND a consumer
     if consumer:
         consumer.kill()
         del _consumers[user]
 
-        if producer:
-            producer.remove_client(user)
+        # Kill the producer
+        if tracking:
+            terms = frozenset(tracking)
+            producer = _producers.get(terms, None)
+
+            if producer:
+                producer.remove_client(user)
+
+                # Delete producer if it's been killed
+                if producer.successful():
+                    print "Deleting the producer."
+                    del _producers[terms]
+            else:
+                print "No producer to kill found for this user."
+
+            # Delete the tracking terms for this user
+            del _tracking[user]
+
+        else:
+            print "No tracking terms for this user. No producer to kill."
+    else:
+        print "No consumer for this user. Killing nothing."
 
 
 @app.before_request
@@ -106,17 +128,17 @@ def on_connect():
 
     print "Connected to a socket. sessid: ", request.namespace.socket.sessid
 
-    # Save user namespace (used to get the socketio emit function)
-    _namespaces[session['username']] = request.namespace
-
 @socketio.on('start-stream', namespace='/twitter')
 def on_start_stream(msg):
     """Starts a twitter stream with the terms specifed in the client message."""
 
-    print "starting stream"
+    # Save user namespace (used to get the socketio emit function)
+    _namespaces[session['username']] = request.namespace
+
+    print "Starting stream on: sessid- ", request.namespace.socket.sessid
+
     # Set the session tracking items
-    tracking = msg['tracking']
-    session['tracking'] = tracking
+    _tracking[session['username']] = msg['tracking']
 
     # Create a new Twitter stream Producer/Consumer
     _create_producer()
@@ -127,7 +149,6 @@ def on_pause_stream():
     """Kills the current Twitter stream."""
 
     print "Pausing Twitter stream."
-    # Kill Consumer
     _kill_stream()
 
 
@@ -135,12 +156,12 @@ def on_pause_stream():
 def on_disconnect():
     """Kills the associated Twitter feed Consumer and Producer if no other clients exist."""
 
-    # TODO: There seem to be intermittent disconnect calls that do not actually
-    # result in the client disconnecting. For example, when the tracking page is open,
-    # this callback is called, on_connect [I believe] is NOT called, and the client
-    # still receives Tweets. This should not happen.
-    print "Entered a disconnect."
-    _kill_stream()
+    # TODO: There seem to be intermittent disconnect calls that occur when a client
+    # does not actually disconnect. For example, when the tracking page is open,
+    # this callback is called.
+    print "Entered a disconnect from: sessid- ", request.namespace.socket.sessid
+
+    # _kill_stream()
 
 
 if __name__ == '__main__':
