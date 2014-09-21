230bbb58198733df8d88f56640472d75abbe0b37
Calvin Belden <cbelden@nd.edu>
2014-09-21 13:06:00 -0500

    Corrected lib mapping in main.js

--- static/js/main.js
+++ static/js/main.js
@@ -3,7 +3,7 @@ require.config({
     paths: {
         jquery: 'libs/jquery/jquery-2.1.1.min',
         underscore: 'libs/underscore/underscore-min',
-        backbone: 'libs/backbone/backbone-debug',
+        backbone: 'libs/backbone/backbone-min',
         socketio: [
                 'http://cdnjs.cloudflare.com/ajax/libs/socket.io/0.9.16/socket.io.min',
                 'libs/socketio/socket.io'
